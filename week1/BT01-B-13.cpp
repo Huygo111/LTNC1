50e8dab2c9a1ba884978a7b2b827613089014e6e
Tran ngoc Huy <tranngochuyhnams@gmail.com>
2023-02-23 23:30:36 +0700

    1st

---
+++ week1/BT01-B-13.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+	int x, y;
+	cin >> x >> y;
+	cout << (double)sqrt(1.0*x*x + 1.0*y*y);
+	return 0;
+}
